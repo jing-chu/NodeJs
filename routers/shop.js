8eb84f4dd0fb1e8368c694ca0557da68b7b0a685
xiongjieyi <jinjingcs@gmail.com>
2020-10-12 21:30:39 +0200

    SQL by sequelize

--- routers/shop.js
+++ routers/shop.js
@@ -19,8 +19,9 @@ router.post('/cart', shopController.postCart)
 
 router.post('/cart-delete-item', shopController.postDeleteCartProduct)
 
+router.post('/create-order', shopController.postOrder)
+
 router.get('/orders',shopController.getOrders)
 
-router.get('/checkout', shopController.getCheckout)
 
 exports.router = router
