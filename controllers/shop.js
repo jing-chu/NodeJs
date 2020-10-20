dea3fa24133e043b6aebbca22d9437dfa630794b
xiongjieyi <jinjingcs@gmail.com>
2020-10-20 11:15:55 +0200

    orders

--- controllers/shop.js
+++ controllers/shop.js
@@ -68,7 +68,7 @@ exports.postCart = (req, res, next) => {
     .catch(err => {console.log(err)})
 }
 
-/**
+
 exports.postDeleteCartProduct = (req, res, next) => {
   const prodId = req.body.productId
   req.user
@@ -78,31 +78,10 @@ exports.postDeleteCartProduct = (req, res, next) => {
   })
   .catch(err => console.log(err))    
 }
- */
+
 exports.postOrder = (req, res, next) => {
-  let fetchedCart
   req.user
-    .getCart()
-    .then(cart => {
-      fetchedCart = cart
-      return cart.getProducts()
-    })
-    .then(products => {
-      return req.user
-        .createOrder()
-        .then(order => {
-          return order.addProducts(
-            products.map(product => {
-              product.orderItem = { quantity: product.cartItem.quantity }
-              return product
-            })
-          )
-        })
-        .catch(err => console.log(err))
-    })
-    .then(result => {
-      return fetchedCart.setProducts(null) 
-    })
+    .addOrder()
     .then(result => {
       res.redirect('/orders')
     })
@@ -110,7 +89,7 @@ exports.postOrder = (req, res, next) => {
 }
 
 exports.getOrders = (req, res, next) => {
-  req.user.getOrders({include: ['products']})
+  req.user.getOrder()
     .then(orders => {
       res.render('shop/orders', {
         pageTitle: 'Your Orders',
--- models/user.js
+++ models/user.js
@@ -68,7 +68,7 @@ class User {
       })
   }
 
-  /*
+
   deleteItemFromCart(productId) {
     const updatedCartItems = this.cart.items.filter(item => {
       return item.productId.toString() !== productId.toString()
@@ -81,7 +81,41 @@ class User {
         {$set: {cart: {items: updatedCartItems}}}
       )
   }
-*/
+
+  addOrder() {
+    const db = getDb()
+    return this.getCart()
+     .then(products => {
+       const order = {
+         items: products,
+         user: {
+           _id: new ObjectId(this._id),
+           name: this.name
+         }
+       }
+      return db
+      .collection('orders')
+      .insertOne(order)  //insert the cart in "orders" collection
+     })
+      .then(result => {
+        this.cart = {items: []}     //clear the variable
+        return db
+          .collection('users')
+          .updateOne(
+            {_id: new ObjectId(this._id)},
+            {$set: {cart: {items: []}}}    //clear it in the database
+          )
+      })
+  }
+
+  getOrder() {
+    const db = getDb()
+    return db
+      .collection('orders')
+      .find({'user._id': new ObjectId(this._id)})
+      .toArray()
+  }
+
   static findById(userId) {
     const db = getDb()
     return db
