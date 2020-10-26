477cc4ce978185bf48c1d94524463892ac5fa7e0
xiongjieyi <jinjingcs@gmail.com>
2020-10-26 20:51:48 +0100

    csrf protecton

--- app.js
+++ app.js
@@ -5,6 +5,7 @@ const bodyParser = require('body-parser') //import body-parser: a third part mod
 const mongoose = require('mongoose')
 const session = require('express-session')
 const MongoDBStore = require('connect-mongodb-session')(session)
+const csrf = require('csurf')  //csrf Protection
 
 const errorController = require('./controllers/error')
 const User = require('./models/user')
@@ -16,6 +17,7 @@ const store = new MongoDBStore({
   uri: MONGODB_URI,
   collection: 'sessions'
 })
+const csrfProtection = csrf()
 
 app.set('view engine','ejs')
 app.set('views','views')
@@ -40,8 +42,14 @@ app.use((req, res, next) => {
 app.use(bodyParser.urlencoded({extended:false}))
 app.use(express.static(path.join(__dirname, 'public')))
 app.use(
-  session({secret: 'my secret', resave: false, saveUninitialized: false, store: store})
+  session({
+    secret: 'my secret', 
+    resave: false, 
+    saveUninitialized: false, 
+    store: store})
 )
+app.use(csrfProtection)
+
 
 app.use((req, res, next) => {
   if (!req.session.user) {
@@ -53,6 +61,12 @@ app.use((req, res, next) => {
    next()
  })
   .catch(err => console.log(err))  
+})
+
+app.use((req, res, next) => {
+  res.locals.isAuthenticated = req.session.isLoggedIn,
+  res.locals.csrfToken = req.csrfToken()
+  next()
 })
 
 // routers middleware
